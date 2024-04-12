f6dd2a98ed2ab3a5895ff67c6d91692dab3ab198
MaureenKwaku <seyramablakwaku@gmail.com>
2024-04-11 19:26:31 -0700

    update task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,8 @@ class Rectangle {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
 	    return ('Rectangle' + {});
     }
-      this.width = w;
-      this.height = h;
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
